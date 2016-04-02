6ad931e7020cc260063b9ed053bd7e1261e4d7cc
evgeniyraev <evgeni.raev@gmail.com>
2016-04-02 19:59:16 +0300

    initial commit

---
+++ Chase (tvOS)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chase (tvOS)
+//
+//  Created by Evgeniy Raev on 4/2/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
